94201500a88f028a75b2697d45b39f58eb13b056
wetPaint99 <benedictjoseph67@gmail.com>
2023-07-25 06:47:42 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ void print_rev(char *s)
 	int i;
 	int count = 0;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
 	for (i = count - 1 ; i >= 0 ; i--)
 		_putchar(s[i]);
