d35d638b69ea62a815864dad480cf00dfe3ca36a
wetPaint99 <benedictjoseph67@gmail.com>
2023-07-24 21:28:55 -0700

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap two interger
+ * @a: value1
+ * @b: value2
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
