38a297be16fa0f7fe67e343d321eeec31efb31ff
wetPaint99 <benedictjoseph67@gmail.com>
2023-07-25 05:41:24 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen - finds the length of a string
- * @s: string to be counted
- * Return: length of the string
+ * _strlen - Finds the length of a string.
+ * @s: The input string
+ * Return: Length of the string.
  */
 int _strlen(char *s)
 {
-    int i;
-    int count = 0;
-
-    for (i = 0; s[i] != '\0'; i++)
-        count++;
-    return count;
+		int i;
+		int count = 0;
+	
+		for (i = 0; s[i] != '\0'; i++)
+			count++;
+	
+		return (count);
 }
+
 
