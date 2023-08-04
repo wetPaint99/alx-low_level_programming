5e77e2edd5b0af8344bc6a4c62d44acf51716095
wetPaint99 <benedictjoseph67@gmail.com>
2023-08-03 19:28:04 -0700

    ./1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string
+ * @s: string to be printed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+
+}
