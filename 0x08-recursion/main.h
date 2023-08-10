1de1b2f5e417a906db910bd1b16a3d3c92db2c9a
wetPaint99 <benedictjoseph67@gmail.com>
2023-08-09 19:29:26 -0700

    Updated

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2)
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
